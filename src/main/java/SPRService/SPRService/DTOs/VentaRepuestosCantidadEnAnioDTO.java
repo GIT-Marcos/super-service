06fd6c38c924cedf43b100de1bd4ea20749caeb5
marcos <m.pardo.agustin@gmail.com>
2025-10-08 17:05:27 -0300

    + faltó una clase xd.

---
+++ src/main/java/SPRService/SPRService/DTOs/VentaRepuestosCantidadEnAnioDTO.java
@@ -0,0 +1,29 @@
+package SPRService.SPRService.DTOs;
+
+public record VentaRepuestosCantidadEnAnioDTO(int nroMes, String mes, Long cantidadVentas) {
+
+    public VentaRepuestosCantidadEnAnioDTO{}
+
+    public VentaRepuestosCantidadEnAnioDTO(int nroMes, Long cantidadVentas) {
+        this(nroMes, buscaMes(nroMes), cantidadVentas);
+    }
+
+    // TODO: esto se repite hacer enum de utilidad
+    private static String buscaMes(int nroMes) {
+        return switch (nroMes) {
+            case 1 -> "Enero";
+            case 2 -> "Febrero";
+            case 3 -> "Marzo";
+            case 4 -> "Abril";
+            case 5 -> "Mayo";
+            case 6 -> "Junio";
+            case 7 -> "Julio";
+            case 8 -> "Agosto";
+            case 9 -> "Septiembre";
+            case 10 -> "Octubre";
+            case 11 -> "Noviembre";
+            case 12 -> "Diciembre";
+            default -> "Mes inválido";
+        };
+    }
+}
