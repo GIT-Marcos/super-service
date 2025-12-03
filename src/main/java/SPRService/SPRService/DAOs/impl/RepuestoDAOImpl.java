0833c1e247bbd27f0cbeb6fb554080ce4c3985a2
marcos <m.pardo.agustin@gmail.com>
2025-12-03 20:31:13 -0300

    fix(repuesto): orden por nombre en ver todos

--- src/main/java/SPRService/SPRService/DAOs/impl/RepuestoDAOImpl.java
+++ src/main/java/SPRService/SPRService/DAOs/impl/RepuestoDAOImpl.java
@@ -43,7 +43,8 @@ public class RepuestoDAOImpl extends GenericDAOImpl<Repuesto, Long> implements R
     public List<Repuesto> todosProductosActivos() {
         EntityManager em = emProvider.get();
         return em.createQuery("SELECT DISTINCT r FROM Repuesto r " +
-                        "WHERE r.activo = true",
+                        "WHERE r.activo = true " +
+                        "ORDER BY r.detalle",
                 Repuesto.class).getResultList();
     }
 
