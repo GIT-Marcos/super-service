ff2117a5452953644d90304f0a492c4c9de2e289
marcos <m.pardo.agustin@gmail.com>
2026-01-19 18:31:24 -0300

    agregados campos a interfaz y controlador

--- src/main/java/SPRService/SPRService/controllers/DetalleVehiculoController.java
+++ src/main/java/SPRService/SPRService/controllers/DetalleVehiculoController.java
@@ -1,12 +1,20 @@
 package SPRService.SPRService.controllers;
 
+import SPRService.SPRService.components.CeldaOperacionUniversal;
 import SPRService.SPRService.entities.Vehiculo;
 import SPRService.SPRService.navigation.DataReceiver;
+import SPRService.SPRService.viewModels.celdas.ItemOperacionViewModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -14,36 +22,54 @@ import java.util.ResourceBundle;
 
 public class DetalleVehiculoController implements Initializable, DataReceiver<Vehiculo> {
 
+    private ObservableList<ItemOperacionViewModel> items = FXCollections.observableArrayList();
+
     @FXML
-    private Label lblPatente;
-    @FXML
-    private Label lblColor;
-    @FXML
-    private Label lblNroChasis;
-    @FXML
-    private Label lblNroMotor;
-    @FXML
-    private Label lblMarca;
-    @FXML
-    private Label lblModelo;
-    @FXML
-    private Label lblAnio;
-    @FXML
-    private Label lblCilindrada;
-    @FXML
-    private Label lblFechaRegistro;
+    private Label lblPatente, lblColor, lblNroChasis, lblNroMotor, lblMarca, lblModelo, lblAnio, lblCilindrada,
+            lblFechaRegistro;
     @FXML
     private ImageView imgMarca;
-
+    @FXML
+    private ListView<ItemOperacionViewModel> lvServices;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        configurarLista();
     }
 
     @Override
     public void receiveData(Vehiculo data) {
-        if (data == null) return;
+        if (data != null) {
+            cargarLabels(data);
+            cargarLista(data);
+        }
+    }
+
+    @FXML
+    private void verDetalle() {
+
+    }
+
+    @FXML
+    private void nuevoService() {
+
+    }
+
+    @FXML
+    private void cerrarVentana(ActionEvent event) {
+        Node n = ((Node) event.getSource());
+        Stage s = (Stage) n.getScene().getWindow();
+        s.close();
+    }
+
+    private void configurarLista() {
+        lvServices.setItems(items);
+        lvServices.setCellFactory(f -> new CeldaOperacionUniversal());
+        String css = getClass().getResource("/styles/celda-operacion.css").toExternalForm();
+        lvServices.getStylesheets().add(css);
+    }
+
+    private void cargarLabels(Vehiculo data) {
         lblPatente.setText(data.getPatente());
         lblColor.setText(data.getColor());
         if (!data.getNroChasis().isEmpty()) lblNroChasis.setText(data.getNroChasis());
@@ -58,5 +84,9 @@ public class DetalleVehiculoController implements Initializable, DataReceiver<Ve
             Image img = new Image(stream);
             imgMarca.setImage(img);
         }
+    }
+
+    private void cargarLista(Vehiculo data) {
+
     }
 }
